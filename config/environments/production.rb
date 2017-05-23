992d0224eb0974bce861799237cfb62c306b71f2
thin <thin.thin.aung@frobom.com>
2017-05-23 09:18:12 +0630

    email success

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,16 +67,16 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
    config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
-  
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.gmail.com',
-    :port           => 587,
-    :authentication => :plain,
-    :user_name      => 'frobosakai@gmail.com',
-    :password       => 'frobosakai123',
-    :domain         => 'gmail.com',
-    :enable_starttls_auto => true
-  }
+config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: frobosakai@gmail.com,
+    password: frobosakai123,
+    authentication: 'plain',
+    enable_starttls_auto: true
+}
+ 
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
