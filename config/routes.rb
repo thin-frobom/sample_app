93f9bec12e52b20753aca77867284da4d7c08ead
thin <thin.thin.aung@frobom.com>
2017-05-23 17:25:07 +0630

    add user following

--- config/routes.rb
+++ config/routes.rb
@@ -21,12 +21,18 @@ Rails.application.routes.draw do
 
   post '/signup', to: 'users#create'
 
-  resources:users
+  resources:users do
+    member do
+      get :following, :followers
+    end
+  end
 
   resources:account_activations,only:[:edit]
 
   resources:password_resets,only: [:new, :create, :edit, :update]
 
   resources:microposts,only: [:create, :destroy]
+
+  resources:relationships,only: [:create, :destroy]
 
 end
